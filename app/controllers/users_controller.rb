4404164c9f8f03d5081f946307c008f15db409dc
Josh Breault <j.breault@live.com>
2015-11-05 17:58:27 -0500

    refactored user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,30 +14,24 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
 
-    respond_to do |format|
-      format.html {redirect_to root, notice: 'User was successfully destroyed'}
-    end
+    redirect_to root_path, notice: 'User was successfully destroyed'
   end
 
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html{redirect_to @user, notice: 'User was successfully created.'}
-      else
-        format.html{render :new}
-      end
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html{redirect_to @user, notice: 'User was successfully updated.'}
-      else
-        format.html{render :new}
-      end
+    if @user.update(user_params)
+      format.html{redirect_to @user, notice: 'User was successfully updated.'}
+    else
+      format.html{render :new}
     end
   end
 
