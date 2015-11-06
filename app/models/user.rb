168c4c69674180ad3c17d17b6bd7622c9aac0a64
jciancio <jciancio7@gmail.com>
2015-11-06 14:21:41 -0500

    added simple validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
+
+  validates :first_name, :last_name, :address, :zip, presence: :true
+  validates :zip, numericality: { only_integer: true}
+  validates :username, presence: :true, uniqueness: :true
 end
