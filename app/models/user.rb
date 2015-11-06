3edf4bea3b95b5d900be20e2e26fffff4ae00e2d
jciancio <jciancio7@gmail.com>
2015-11-06 14:33:28 -0500

    added email to user and some validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 
   validates :first_name, :last_name, :address, :zip, presence: :true
-  validates :zip, numericality: { only_integer: true}
+  validates :zip, numericality: { only_integer: true}, length: {maximum: 5}
   validates :username, presence: :true, uniqueness: :true
+  validates :email, presence: :true, uniqueness: :true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
