3f62d81aec2bd7579267ccd0cdfaab2206031b15
jciancio <jciancio7@gmail.com>
2015-11-07 16:14:25 -0500

    added function to grab lat and lon and put in an object

--- app/assets/javascripts/doctors.js
+++ app/assets/javascripts/doctors.js
@@ -1,22 +1,41 @@
-var api_key = '9c6f158f207798d47ab9a94c95dfaabc'; // Get your API key at developer.betterdoctor.com
+$(document).ready(function() {
+  var api_key = '9c6f158f207798d47ab9a94c95dfaabc';
 
-var resource_url = 'https://api.betterdoctor.com/2015-01-27/doctors?query=Allergy&specialty_uid=allergist&skip=0&limit=10&user_key=' + api_key;
+  var resource_url = 'https://api.betterdoctor.com/2015-01-27/doctors?query=Allergy&specialty_uid=allergist&skip=0&limit=10&user_key=' + api_key;
 
-$.getJSON( resource_url, function(data) {
-  var list = data.data;
-  var profile = list[0];
-  console.log(list);
-  $('.doc-index').append(template(profile.profile.first_name, profile.profile.title, profile.profile.image_url));
+  $.getJSON( resource_url, function(data) {
+    var latLngInfo = new Object();
+    var list = data.data
+    for (var i = 0; i < list.length; i++) {
+      var practices = list[i].practices;
+      practices.forEach(function(practice) {
+        latLngInfo[practice.name] = {'lat': practice.lat, 'lng': practice.lon};
+      });
+    }
+    console.log(latLngInfo);
+    return latLngInfo;
+  });
 });
 
-function template(name, data, picture) {
-  return ["<tr>",
-            "<td>" + name + "</td>",
-            "<td>" + data + "</td>",
-            "<td>" + picture + "</td>",
-        "</tr>"
-        ].join();
-}
+
+
+
+
+// $.getJSON( resource_url, function(data) {
+//   var list = data.data;
+//   var profile = list[0];
+//   console.log(list[0].practices[0].lat);
+//   $('.doc-index').append(template(profile.profile.first_name, profile.profile.title, profile.profile.image_url));
+// });
+
+// function template(name, data, picture) {
+//   return ["<tr>",
+//             "<td>" + name + "</td>",
+//             "<td>" + data + "</td>",
+//             "<td>" + picture + "</td>",
+//         "</tr>"
+//         ].join();
+// }
 
 // $.ajax({
 //   dataType: "json",
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # before_action :check_logged_in, except: [:welcome, :new]
+
+  # def check_logged_in
+  #   @user = User.find_by(params[:email]).try(:authenticate, params[:password])
+
+  #   unless @user
+  #     redirect_to root_path
+  #   end
+  # end
 end
