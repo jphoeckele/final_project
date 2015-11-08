9fcaa58d9427e27319a9128cbbfdafd72fa42bca
jciancio <jciancio7@gmail.com>
2015-11-07 20:03:17 -0500

    we are pulling the lat and lon and putting it into an object, but can't access the object now

--- app/assets/javascripts/doctors.js
+++ app/assets/javascripts/doctors.js
@@ -1,45 +1,89 @@
-$(document).ready(function() {
+$(function() {
   var api_key = '9c6f158f207798d47ab9a94c95dfaabc';
+  var resource_url = 'https://api.betterdoctor.com/2015-01-27/doctors?query=specialty_uid=allergist&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=50&user_key=' + api_key;
 
-  var resource_url = 'https://api.betterdoctor.com/2015-01-27/doctors?query=Allergy&specialty_uid=allergist&skip=0&limit=10&user_key=' + api_key;
+  // Returns an object {Practice Name: {'lat' : 123, 'lng' : 123, } Practice Name.....}
+  function getLatLng() {
+    return $.getJSON( resource_url, function(data) {
+      var latLngInfo = new Object();
+      data = latLngInfo;
+    });
+  }
 
-  $.getJSON( resource_url, function(data) {
-    var latLngInfo = new Object();
-    var list = data.data
-    for (var i = 0; i < list.length; i++) {
-      var practices = list[i].practices;
-      practices.forEach(function(practice) {
-        latLngInfo[practice.name] = {'lat': practice.lat, 'lng': practice.lon};
-      });
+  var latlng = getLatLng();
+
+  // latlng.success(function(data) {
+  //   var latLngInfo = new Object();
+  //   var list = data.data
+  //   for (var i = 0; i < list.length; i++) {
+  //     var practices = list[i].practices;
+  //     practices.forEach(function(practice) {
+  //       latLngInfo[practice.name] = {'lat': practice.lat, 'lon': practice.lon};
+  //     });
+  //   }
+  //   console.log(latLngInfo);
+  // });
+
+
+
+  // GOOGLE MAPS API
+  function initialize() {
+
+    latlng.success(function(data) {
+      var latLngInfo = new Object();
+      var list = data.data
+      for (var i = 0; i < list.length; i++) {
+        var practices = list[i].practices;
+        practices.forEach(function(practice) {
+          latLngInfo[practice.name] = {'lat': practice.lat, 'lon': practice.lon};
+        });
+      }
+      console.log(latLngInfo);
+      for (var i = 0; i < 50; i++ ) {
+        var lat = latLngInfo;
+        var lon = latLngInfo;
+        locations.push ( {name:"BUG", latlng: new google.maps.LatLng(lat, lon)});
+      }
+    });
+
+    var mapCanvas = document.getElementById('map');
+
+    var mapOptions = {
+      center: new google.maps.LatLng(37.773, -122.413),
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+
+    var locations = [];
+
+    // for (var i = 0; i < 50; i++ ) {
+    //   locations.push ( {name:"BUG", latlng: new google.maps.LatLng(test, test2)})
+    // }
+
+    for(var i=0; i<locations.length; i++ ) {
+      var marker = new google.maps.Marker({position: locations[i].latlng,map:map, title:locations[i].name});
     }
-    console.log(latLngInfo);
-    return latLngInfo;
-  });
+
+
+    // var point = new google.maps.LatLng(25.801579, -80.202176);
+
+    // var marker = new google.maps.Marker({
+    //     position:point,
+    //     map:map,
+    // })
+
+
+    var contentstring = '<div> Doctor <br> hello <br> sup </div>';
+    var infowindow = new google.maps.InfoWindow({
+        content:contentstring
+        })
+    google.maps.event.addListener(marker,'click',function(){
+      infowindow.open(map,marker);
+    });
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
 });
-
-
-
-
-
-// $.getJSON( resource_url, function(data) {
-//   var list = data.data;
-//   var profile = list[0];
-//   console.log(list[0].practices[0].lat);
-//   $('.doc-index').append(template(profile.profile.first_name, profile.profile.title, profile.profile.image_url));
-// });
-
-// function template(name, data, picture) {
-//   return ["<tr>",
-//             "<td>" + name + "</td>",
-//             "<td>" + data + "</td>",
-//             "<td>" + picture + "</td>",
-//         "</tr>"
-//         ].join();
-// }
-
-// $.ajax({
-//   dataType: "json",
-//   url: resource_url,
-//   data: data,
-//   success: success
-// });
