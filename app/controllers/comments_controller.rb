64f02cf0ff71dc3ac022c9ba3780e36c43de365e
jciancio <jciancio7@gmail.com>
2015-11-14 14:27:25 -0500

    got the comments attached to the messages, working on getting the flow correct a.k.a UX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_message, only: [:new, :create]
 
   # GET /comments
   # GET /comments.json
@@ -24,7 +25,7 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @message.comments.new(comment_params)
 
     respond_to do |format|
       if @comment.save
@@ -65,6 +66,10 @@ class CommentsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
+    end
+
+    def set_message
+      @message = Message.find(params[:message_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
